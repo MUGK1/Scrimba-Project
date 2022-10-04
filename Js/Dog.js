f4c24c034c34a11b7030068bb822be788348be29
Khaled Almansour <Khaled@mugendai.io>
2022-10-04 22:58:59 +0300

    Huge Improvement

--- Js/Dog.js
+++ Js/Dog.js
@@ -1,1 +1,28 @@
 // Create the Dog class here
+
+class Posters {
+  constructor(data) {
+    Object.assign(this, data);
+  }
+
+  getPosterHTML() {
+    const { name, avatar, age, bio } = this;
+    return `
+    <div class="mainContent">
+        <img
+        src="images/badge-like.png"
+        alt="Like Stamp"
+        class="mainContent__stamp none"
+        />
+        <img
+        src="${avatar}"
+        alt="A dog Picture"
+        class="mainContent__img"
+        />
+        <h1 class="mainContent__header">${name}, ${age}</h1>
+        <p class="mainContent__para">${bio}</p>
+    </div> `;
+  }
+}
+
+export default Posters;
--- Js/index.js
+++ Js/index.js
@@ -1,1 +1,19 @@
 // Remember to import the data and Dog class!
+import Posters from "./Dog.js";
+import dogs from "./data.js";
+
+const btnLike = document.getElementById("like");
+const btnDis = document.getElementById("dis");
+const posters = document.getElementById("mainContent");
+
+const rexDog = new Posters(dogs.rex);
+const bellaDog = new Posters(dogs.Bella);
+const teddyDog = new Posters(dogs.Teddy);
+
+function render() {
+  posters.innerHTML = bellaDog.getPosterHTML();
+}
+
+btnLike.addEventListener("click", function () {
+  render();
+});
